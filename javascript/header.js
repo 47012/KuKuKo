1552f57f5b03562bd19f6666d5a374672eb52e7d
KuosaKuorsaaKovaa <124239348+KuosaKuorsaaKovaa@users.noreply.github.com>
2023-03-09 12:57:12 +0200

    v3.17

--- javascript/header.js
+++ javascript/header.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function() {
   const dropdownBtn = document.querySelector('.dropbtn');
   const dropdownContent = document.querySelector('.dropdown-content');
