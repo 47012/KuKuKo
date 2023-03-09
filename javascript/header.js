fb2d4047310821432bf6cb9b3b9dd7f23119f814
KuosaKuorsaaKovaa <124239348+KuosaKuorsaaKovaa@users.noreply.github.com>
2023-03-09 12:39:31 +0200

    v3.14

--- javascript/header.js
+++ javascript/header.js
@@ -1,3 +1,4 @@
+const hiframe = document.getElementById("hiframe");
 const dropdownBtn = document.querySelector('.dropbtn');
 const dropdownContent = document.querySelector('.dropdown-content');
 const dropdown = document.querySelector('.dropdown');
