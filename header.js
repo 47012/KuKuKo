3e28e7576a98490651c0b5ad76258893f9eb4e87
KuosaKuorsaaKovaa <124239348+KuosaKuorsaaKovaa@users.noreply.github.com>
2023-03-09 10:59:47 +0200

    v3.7

---
+++ header.js
@@ -0,0 +1,7 @@
+const dropdownBtn = document.querySelector('.dropbtn');
+const dropdownContent = document.querySelector('.dropdown-content');
+const dropdown = document.querySelector('.dropdown');
+
+dropdownBtn.addEventListener('click', function() {
+  dropdown.classList.toggle('active');
+});
